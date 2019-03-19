e980a2db906f27d9abdf5b7f815633404a6be935
Kazuto Ando <kazuto.ando@gmail.com>
2019-03-19 21:44:58 +0900

    first commit

---
+++ google.rb
@@ -0,0 +1,15 @@
+require 'nokogiri'
+require 'open-uri'
+require 'uri'
+require 'cgi'
+
+
+escaped_url = URI.escape("https://www.google.com/search?q=株価")
+doc = Nokogiri::HTML(open(escaped_url))
+
+puts doc.xpath("//*[@id='resultStats']/text()")
+
+doc.xpath('//h3/a').each do |link|
+    puts CGI.parse(link[:href])["adurl"]
+    puts link.content
+end
